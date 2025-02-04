4405def0aa235d3b743790035b230e559cc10812
Gabriel Santos <gabrildado08@gmail.com>
2025-02-04 08:22:21 -0300

    ladingpage

--- resources/js/Components/HeaderComponent.jsx
+++ resources/js/Components/HeaderComponent.jsx
@@ -4,8 +4,8 @@ import React, {useContext} from "react";
 export default function HeaderComponent({auth}) {
     return (
         <>
-            <div className="flex border-bottom border-warning border-4 h-5rem p-4 header justify-content-between align-items-center w-100"
-                 style={{zIndex: '999999'}}>
+            <div className="flex position-fixed top-0 border-bottom border-warning border-4 h-5rem p-4 header justify-content-between align-items-center w-100"
+                 style={{zIndex: '999999', background: '#00c3ea'}}>
                 <img width={55} src='/images/home/icons/logo.jpg' alt='box'/>
                 <div className="flex align-items-center gap-5">
                     <Link className='btn btn-warning' href='/'>Entre em contato</Link>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,8 +1,24 @@
 import {Link, Head} from '@inertiajs/react';
 import {LayoutContext, LayoutProvider} from "@/Layouts/layout/context/layoutcontext.jsx";
 import {PrimeReactProvider} from "primereact/api";
-import React, {useContext} from "react";
-import { FaFacebookF,FaBullhorn, FaHandshake, FaChartLine, FaTwitter, FaGoogle, FaInstagram, FaLinkedin, FaGithub, FaGem, FaHome, FaEnvelope, FaPhone, FaPrint } from "react-icons/fa";
+import React, {useContext, useState} from "react";
+import {
+    FaFacebookF,
+    FaBullhorn,
+    FaHandshake,
+    FaChartLine,
+    FaTwitter,
+    FaGoogle,
+    FaInstagram,
+    FaLinkedin,
+    FaGithub,
+    FaGem,
+    FaHome,
+    FaEnvelope,
+    FaPhone,
+    FaPrint,
+    FaPhoneAlt, FaWhatsapp, FaMapMarkerAlt
+} from "react-icons/fa";
 import HeaderComponent from "@/Components/HeaderComponent.jsx";
 import {motion} from "framer-motion";
 import {useInView} from "react-intersection-observer";
@@ -10,6 +26,8 @@ import ProductsComponent from "@/Components/ProductsComponent.jsx";
 import CarouselComponent from "@/Components/CarouselComponent.jsx";
 
 export default function Welcome({auth, laravelVersion, phpVersion}) {
+    const [scale, setScale] = useState(1);
+
     const plans = [
         {
             title: 'Standard plan',
@@ -29,20 +47,20 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
     ];
 
     const cardVariants = {
-        hidden: { opacity: 0, rotate: -10, scale: 0.8 },
+        hidden: {opacity: 0, rotate: -10, scale: 0.8},
         visible: {
             opacity: 1,
             rotate: 0,
             scale: 1,
-            transition: { type: "spring", stiffness: 120, damping: 12 }
+            transition: {type: "spring", stiffness: 120, damping: 12}
         }
     };
 
     const cards = [
-        { id: 1, icon: <FaBullhorn />, title: "Digital Marketing" },
-        { id: 2, icon: <FaChartLine />, title: "Consulting Services" },
-        { id: 3, icon: <FaChartLine />, title: "Consulting Services" },
-        { id: 4, icon: <FaHandshake />, title: "Business Solutions" }
+        {id: 1, icon: <FaBullhorn/>, title: "Digital Marketing"},
+        {id: 2, icon: <FaChartLine/>, title: "Consulting Services"},
+        {id: 3, icon: <FaChartLine/>, title: "Consulting Services"},
+        {id: 4, icon: <FaHandshake/>, title: "Business Solutions"}
     ];
 
     return (
@@ -52,15 +70,52 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
                     <Head title="Welcome"/>
                     <HeaderComponent auth={auth}/>
 
-                    <section className="container-slider w-100 border-bottom border-warning border-4">
-                        <img className='w-full img-fluid' src='/images/home/arquitetura.jpg' alt=''
-                             style={{maxHeight: '45rem'}}/>
+                    <section
+                        className="w-100 flex align-items-center p-8"
+                        style={{
+                            marginTop: "2rem",
+                            backgroundImage: "url('/images/home/fundo.jpg')",
+                        }}
+                    >
+                        <div className="container mx-auto flex md:flex-row flex-column gap-6 align-items-center"
+                             style={{marginTop: "3rem"}}>
+                            {/* Texto */}
+                            <motion.div
+                                className="flex flex-column gap-6 justify-content-center align-items-center"
+                                initial={{opacity: 0, y: 50}}
+                                animate={{opacity: 1, y: 0}}
+                                transition={{duration: 0.8}}
+                            >
+                                <h1 className="text-center">Cuidando da Sua Saúde com Excelência e Humanização</h1>
+                                <motion.a
+                                    className="btn btn-warning"
+                                    whileHover={{scale: 1.1}}
+                                    whileTap={{scale: 0.9}}
+                                >
+                                    Entre em contato
+                                </motion.a>
+                            </motion.div>
+
+                            {/* Imagem */}
+                            <motion.div
+                                initial={{opacity: 0, x: 50}}
+                                animate={{opacity: 1, x: 0}}
+                                transition={{duration: 0.8, delay: 0.2}}
+                            >
+                                <img
+                                    className="w-full img-fluid"
+                                    src="/images/home/medical.jpg"
+                                    alt=""
+                                    style={{maxHeight: "45rem", borderRadius: "20px"}}
+                                />
+                            </motion.div>
+                        </div>
                     </section>
 
                     <ProductsComponent data={plans}/>
 
-                    <section className='w-full p-4 pt-8 pb-8 mt-8 bg-warning'>
-                        <div className='container flex justify-content-center align-items-center'>
+                    <section className='w-full p-5 mt-8 ' style={{background: '#00c3ea'}}>
+                        <div className=' flex justify-content-center align-items-center'>
                             <CarouselComponent/>
                         </div>
                     </section>
@@ -94,6 +149,71 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
                             </div>
                         </div>
                     </section>
+
+                    <section className='mt-8 p-7' style={{backgroundImage: "url('/images/home/fundo.jpg')"}}>
+                        <div className="container mx-auto mt-5">
+                            <div className="flex justify-content-center align-items-center flex-wrap gap-8 ">
+                                {/* Informações de Contato */}
+                                <div className="card p-4 shadow-lg">
+                                    <h2 className="mb-4">Entre em Contato</h2>
+                                    <p>
+                                        <FaMapMarkerAlt className="text-danger"/> <strong>Endereço:</strong> Rua
+                                        Exemplo, 123 - São Paulo, SP
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt className="text-primary"/> <strong>Telefone:</strong> <a
+                                        href="tel:+5511999999999">(11) 99999-9999</a>
+                                    </p>
+                                    <p>
+                                        <FaEnvelope className="text-warning"/> <strong>Email:</strong> <a
+                                        href="mailto:contato@empresa.com">contato@empresa.com</a>
+                                    </p>
+                                    <a href="https://wa.me/5511999999999" className="btn btn-success">
+                                        <FaWhatsapp/> Fale pelo WhatsApp
+                                    </a>
+                                </div>
+
+
+                                {/* Google Maps */}
+                                <div className="ratio ratio-16x9 rounded shadow-lg " style={{width: '60%'}}>
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509366!2d144.95373531590465!3d-37.81627974202102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d5df0f56eaf%3A0x6f34a5c8b6f9e!2sMelbourne%2C%20VIC%2C%20Austr%C3%A1lia!5e0!3m2!1spt-BR!2sbr!4v1648903832123!5m2!1spt-BR!2sbr"
+                                        allowFullScreen
+                                        loading="lazy"
+                                        title="Google Maps"
+                                    ></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <a
+                        href="https://wa.me/5511999999999"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="float-btn"
+                        style={{
+                            position: "fixed",
+                            bottom: "20px",
+                            right: "20px",
+                            zIndex: 9999,
+                            width: "80px",
+                            height: "80px",
+                            borderRadius: "50%",
+                            backgroundColor: "#25D366",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+                            transition: "transform 0.6s ease-in-out",
+                            transform: `scale(${scale})`,
+                        }}
+                        onMouseEnter={() => setScale(1.1)}
+                        onMouseLeave={() => setScale(1)}
+                    >
+                        <img src="/images/home/icons/whatsapp.png" width={50} alt="WhatsApp"/>
+                    </a>
+
                     <footer className="text-center mt-8 text-lg-start bg-body-tertiary text-muted">
                         <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
                             <div className="me-5 d-none d-lg-block">
@@ -113,7 +233,7 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
                                     <FaInstagram/>
                                 </a>
                                 <a href="#" className="me-4 text-reset">
-                                <FaLinkedin/>
+                                    <FaLinkedin/>
                                 </a>
                                 <a href="#" className="me-4 text-reset">
                                     <FaGithub/>
